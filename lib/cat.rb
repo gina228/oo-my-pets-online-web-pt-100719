2fc43821d935d78a0797975877ce2e8639fea5d9
Gina Graci <ginagraci92@gmail.com>
2019-11-08 03:29:44 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,9 +1,10 @@
 class Cat
- attr_reader :name
+ attr_reader :name, :owner
   attr_accessor :mood
 
-  def initialize(name, mood = "nervous")
+  def initialize(name, mood = "nervous", owner)
     @mood = mood
     @name = name
+    @owner = owner
   end
 end
